5fb921ad000c8c5031fdbf473f793282e1f84d07
Pablo Donato <pablo.donato@tuta.io>
2023-07-20 12:31:10 +0200

    Fix biber version problem in nix flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,27 @@
   };
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    tectonic = pkgs.tectonic;
-  in {
-    devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = [ tectonic ];
+    let
+      system = "x86_64-linux";
+
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      pkgs_biber = import
+        (builtins.fetchGit {
+          # Descriptive name to make the store path easier to identify                
+          name = "nixpkgs-biber-2.14";
+          url = "https://github.com/NixOS/nixpkgs/";
+          ref = "refs/heads/nixpkgs-unstable";
+          rev = "3913f6a514fa3eb29e34af744cc97d0b0f93c35c";
+        })
+        { inherit system; };
+
+      tectonic = pkgs.tectonic;
+      biber = pkgs_biber.biber;
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        packages = [ tectonic biber ];
+      };
     };
-  };
 }
